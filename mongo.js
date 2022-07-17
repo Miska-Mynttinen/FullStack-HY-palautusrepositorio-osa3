d2f6b2d358934ec767ef4c5424b038ce92e62e6f
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2022-07-17 17:18:15 +0300

    3.22 lint-konfiguraatio

--- mongo.js
+++ mongo.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
-} 
+}
 
 const password = process.argv[2]
 
-const url = 
+const url =
   `mongodb+srv://miska_mongo:${password}@cluster0.cbuze.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.connect(url)
@@ -29,14 +29,14 @@ const person = new Person({
 Kun muoto on pidempi ja command: node mongo.js yourpassword "Arto Vihavainen" 040-1234556, tai jokin samanlainen niin lisää henkilön tiedot tietokantaan.
 Palauttaa added Arto Vihavainen 040-1234556 to phonebook*/
 if (process.argv.length === 5) {
-  person.save().then(result => {
+  person.save().then(() => {
     console.log(`added ${person.name} number ${person.number} to phonebook!`)
     mongoose.connection.close()
   })
 }
 
 if (process.argv.length === 3) {
-Person.find({}).then(result => {
+  Person.find({}).then(result => {
     console.log('phonebook:')
     result.forEach(person => {
       console.log(`${person.name} ${person.number}`)
